68a9d3385d5b471c25859dff9672e51cc9860c8e
DanSuRii <dansuri2@gmail.com>
2016-06-24 11:41:35 +0200

    ProjectAdd

---
+++ MultipleExcelSearch/MultipleExcelSearch/Form1.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace MultipleExcelSearch
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuchenBegriff = new System.Windows.Forms.TextBox();
+            this.SuchenText = new System.Windows.Forms.Label();
+            this.btnSuchen = new System.Windows.Forms.Button();
+            this.listFiles = new System.Windows.Forms.ListView();
+            this.FileName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Path = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.listResult = new System.Windows.Forms.ListView();
+            this.File = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Reference = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnAddFile = new System.Windows.Forms.Button();
+            this.btnAddFolder = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.treeResult = new System.Windows.Forms.TreeView();
+            this.bMultiThread = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // SuchenBegriff
+            // 
+            this.SuchenBegriff.Location = new System.Drawing.Point(112, 68);
+            this.SuchenBegriff.Name = "SuchenBegriff";
+            this.SuchenBegriff.Size = new System.Drawing.Size(174, 22);
+            this.SuchenBegriff.TabIndex = 0;
+            // 
+            // SuchenText
+            // 
+            this.SuchenText.AutoSize = true;
+            this.SuchenText.Location = new System.Drawing.Point(23, 68);
+            this.SuchenText.Name = "SuchenText";
+            this.SuchenText.Size = new System.Drawing.Size(83, 17);
+            this.SuchenText.TabIndex = 1;
+            this.SuchenText.Text = "SuchenText";
+            // 
+            // btnSuchen
+            // 
+            this.btnSuchen.Location = new System.Drawing.Point(292, 67);
+            this.btnSuchen.Name = "btnSuchen";
+            this.btnSuchen.Size = new System.Drawing.Size(96, 23);
+            this.btnSuchen.TabIndex = 2;
+            this.btnSuchen.Text = "doSuchen";
+            this.btnSuchen.UseVisualStyleBackColor = true;
+            this.btnSuchen.Click += new System.EventHandler(this.btnSuchen_Click);
+            // 
+            // listFiles
+            // 
+            this.listFiles.AllowDrop = true;
+            this.listFiles.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.FileName,
+            this.Path});
+            this.listFiles.Location = new System.Drawing.Point(26, 201);
+            this.listFiles.Name = "listFiles";
+            this.listFiles.Size = new System.Drawing.Size(391, 188);
+            this.listFiles.TabIndex = 3;
+            this.listFiles.UseCompatibleStateImageBehavior = false;
+            this.listFiles.View = System.Windows.Forms.View.Details;
+            this.listFiles.DragDrop += new System.Windows.Forms.DragEventHandler(this.listFiles_DragDrop);
+            // 
+            // FileName
+            // 
+            this.FileName.Text = "fileName";
+            this.FileName.Width = 314;
+            // 
+            // Path
+            // 
+            this.Path.Text = "Path";
+            // 
+            // listResult
+            // 
+            this.listResult.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.File,
+            this.Reference});
+            this.listResult.Location = new System.Drawing.Point(453, 27);
+            this.listResult.Name = "listResult";
+            this.listResult.Size = new System.Drawing.Size(281, 177);
+            this.listResult.TabIndex = 3;
+            this.listResult.UseCompatibleStateImageBehavior = false;
+            this.listResult.View = System.Windows.Forms.View.Details;
+            // 
+            // File
+            // 
+            this.File.Text = "File";
+            // 
+            // Reference
+            // 
+            this.Reference.Text = "Reference";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 178);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "SelectedFile";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(450, 7);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Result";
+            // 
+            // btnAddFile
+            // 
+            this.btnAddFile.Location = new System.Drawing.Point(26, 395);
+            this.btnAddFile.Name = "btnAddFile";
+            this.btnAddFile.Size = new System.Drawing.Size(75, 23);
+            this.btnAddFile.TabIndex = 5;
+            this.btnAddFile.Text = "AddFiles";
+            this.btnAddFile.UseVisualStyleBackColor = true;
+            this.btnAddFile.Click += new System.EventHandler(this.btnAddFile_Click);
+            // 
+            // btnAddFolder
+            // 
+            this.btnAddFolder.Location = new System.Drawing.Point(329, 395);
+            this.btnAddFolder.Name = "btnAddFolder";
+            this.btnAddFolder.Size = new System.Drawing.Size(88, 23);
+            this.btnAddFolder.TabIndex = 5;
+            this.btnAddFolder.Text = "AddFolder";
+            this.btnAddFolder.UseVisualStyleBackColor = true;
+            this.btnAddFolder.Click += new System.EventHandler(this.btnAddFolder_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "Excel Files(*.xls;*.xlsx)|*.xls;*.xlsx";
+            this.openFileDialog1.Multiselect = true;
+            // 
+            // treeResult
+            // 
+            this.treeResult.Location = new System.Drawing.Point(453, 210);
+            this.treeResult.Name = "treeResult";
+            this.treeResult.Size = new System.Drawing.Size(281, 179);
+            this.treeResult.TabIndex = 6;
+            this.treeResult.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.treeResult_MouseDoubleClick);
+            // 
+            // bMultiThread
+            // 
+            this.bMultiThread.AutoSize = true;
+            this.bMultiThread.Location = new System.Drawing.Point(112, 27);
+            this.bMultiThread.Name = "bMultiThread";
+            this.bMultiThread.Size = new System.Drawing.Size(100, 21);
+            this.bMultiThread.TabIndex = 7;
+            this.bMultiThread.Text = "Multithread";
+            this.bMultiThread.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(779, 452);
+            this.Controls.Add(this.bMultiThread);
+            this.Controls.Add(this.treeResult);
+            this.Controls.Add(this.btnAddFolder);
+            this.Controls.Add(this.btnAddFile);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listResult);
+            this.Controls.Add(this.listFiles);
+            this.Controls.Add(this.btnSuchen);
+            this.Controls.Add(this.SuchenText);
+            this.Controls.Add(this.SuchenBegriff);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.Text = "Multiple Excel Sucher";
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.listFiles_DragDrop);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox SuchenBegriff;
+        private System.Windows.Forms.Label SuchenText;
+        private System.Windows.Forms.Button btnSuchen;
+        private System.Windows.Forms.ListView listFiles;
+        private System.Windows.Forms.ColumnHeader FileName;
+        private System.Windows.Forms.ColumnHeader Path;
+        private System.Windows.Forms.ListView listResult;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ColumnHeader File;
+        private System.Windows.Forms.ColumnHeader Reference;
+        private System.Windows.Forms.Button btnAddFile;
+        private System.Windows.Forms.Button btnAddFolder;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TreeView treeResult;
+        private System.Windows.Forms.CheckBox bMultiThread;
+    }
+}
+
---
+++ MultipleExcelSearch/MultipleExcelSearch/Form1.cs
@@ -0,0 +1,472 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Office.Interop;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace MultipleExcelSearch
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAddFile_Click(object sender, EventArgs e)
+        {
+            if (DialogResult.OK == openFileDialog1.ShowDialog())
+            {
+                foreach (string strSelect in openFileDialog1.FileNames)
+                {
+                    String strFileName = System.IO.Path.GetFileName(strSelect);
+                    String strDirectory = System.IO.Path.GetDirectoryName(strSelect);
+
+                    ListViewItem lvItem = new ListViewItem(strFileName);
+                    lvItem.SubItems.Add(strDirectory);
+                    lvItem.Tag = strSelect;
+
+                    listFiles.Items.Add(lvItem);
+                }
+            }
+
+        }
+
+        private void btnAddFolder_Click(object sender, EventArgs e)
+        {
+            if (DialogResult.OK == folderBrowserDialog1.ShowDialog())
+            {
+                String strPath = folderBrowserDialog1.SelectedPath;
+
+                ListViewItem lvItem = new ListViewItem("*.xls;*.xlsx");
+                lvItem.SubItems.Add(strPath);
+
+                listFiles.Items.Add(lvItem);
+            }
+        }
+
+        private void listFiles_DragDrop(object sender, DragEventArgs e)
+        {
+            listFiles.Items.Add(e.Data.ToString());
+        }
+
+        class TreeNodeExt : TreeNode
+        {
+            string strFilePath;
+            public TreeNodeExt( string ExcelFilePath , TreeNode[] nodeArray )
+                : base(System.IO.Path.GetFileName(ExcelFilePath), nodeArray)
+            {
+                strFilePath = ExcelFilePath;
+            }
+
+            public string GetFilePath() { return strFilePath; }
+        }
+
+        private bool ExcelFindInWorkbook(ref TreeNode toRet, Excel._Workbook oWB, string strToFind)
+        {
+            bool bGeFunden = false;
+
+            List<TreeNode> listGeFunden = new List<TreeNode>();
+
+            foreach (Excel._Worksheet oSheet in oWB.Sheets)
+            {
+                Excel.Range firstFind = null;
+                Excel.Range currentFind = oSheet.Cells.Find(strToFind, Type.Missing
+                , Excel.XlFindLookIn.xlValues, Excel.XlLookAt.xlPart
+                , Excel.XlSearchOrder.xlByRows, Excel.XlSearchDirection.xlNext, false
+                , Type.Missing, Type.Missing);
+
+                while (currentFind != null)
+                {
+                    // Keep track of the first range you find. 
+                    if (firstFind == null)
+                    {
+                        firstFind = currentFind;
+                    }
+
+                    // If you didn't move to a new range, you are done.
+                    else if (currentFind.get_Address(Excel.XlReferenceStyle.xlA1)
+                          == firstFind.get_Address(Excel.XlReferenceStyle.xlA1))
+                    {
+                        break;
+                    }
+
+                    /*
+                    currentFind.Font.Color = System.Drawing.ColorTranslator.ToOle(System.Drawing.Color.Red);
+                    currentFind.Font.Bold = true;
+                    */
+
+                    // TestCode
+                    //var vFind = currentFind.get_Address(Excel.XlReferenceStyle.xlA1);
+                    //currentFind.Select();                    
+                    listGeFunden.Add(new TreeNode(oSheet.Name + "!" + currentFind.get_Address(Excel.XlReferenceStyle.xlA1)));
+
+                    bGeFunden = true;
+
+                    currentFind = oSheet.Cells.FindNext(currentFind);
+                }
+            }
+
+            if (0 < listGeFunden.Count)
+            {
+                TreeNode[] nodeList = listGeFunden.ToArray();
+
+                toRet = new TreeNodeExt(oWB.FullName, nodeList);
+            }
+
+
+            return bGeFunden;
+        }
+
+        private bool ExcelFindInWorkbook(Excel._Workbook oWB, string strToFind)
+        {
+
+            /*
+             TODO: 
+             OpenFileDialog Filter *.xls;*.xlsx
+              
+             MainThread 
+             ...
+             PostQueuedCompletionStatus( SuchenCode, WorkBook or FilePath )              
+              
+             Worker Thread,
+             GetQueuedCompletionStatus
+            {
+                ...Arbeit...
+
+                PostMainthreadResult( TreeNode[] );
+            }
+             */
+
+            TreeNode toRet = null;
+            bool bGeFunden = ExcelFindInWorkbook(ref toRet, oWB, strToFind);
+            if(null != toRet) treeResult.Nodes.Add(toRet);
+
+#if false
+            bool bGeFunden = false;
+
+
+            List<TreeNode> listGeFunden = new List<TreeNode>();
+
+            foreach (Excel._Worksheet oSheet in oWB.Sheets)
+            {
+                Excel.Range firstFind = null;
+                Excel.Range currentFind = oSheet.Cells.Find(strToFind, Type.Missing
+                , Excel.XlFindLookIn.xlValues, Excel.XlLookAt.xlPart
+                , Excel.XlSearchOrder.xlByRows, Excel.XlSearchDirection.xlNext, false
+                , Type.Missing, Type.Missing);
+
+                while (currentFind != null)
+                {
+                    // Keep track of the first range you find. 
+                    if (firstFind == null)
+                    {
+                        firstFind = currentFind;
+                    }
+
+                    // If you didn't move to a new range, you are done.
+                    else if (currentFind.get_Address(Excel.XlReferenceStyle.xlA1)
+                          == firstFind.get_Address(Excel.XlReferenceStyle.xlA1))
+                    {
+                        break;
+                    }
+
+                    /*
+                    currentFind.Font.Color = System.Drawing.ColorTranslator.ToOle(System.Drawing.Color.Red);
+                    currentFind.Font.Bold = true;
+                    */
+
+                    // TestCode
+                    //var vFind = currentFind.get_Address(Excel.XlReferenceStyle.xlA1);
+                    //currentFind.Select();                    
+                    listGeFunden.Add(new TreeNode(oSheet.Name + "!" + currentFind.get_Address(Excel.XlReferenceStyle.xlA1)));
+
+                    bGeFunden = true;
+
+                    currentFind = oSheet.Cells.FindNext(currentFind);
+                }
+            }
+
+            if (0 < listGeFunden.Count)
+            {
+                TreeNode[] nodeList = listGeFunden.ToArray();
+
+                TreeNode treeNode = new TreeNodeExt(oWB.FullName, nodeList);
+                treeResult.Nodes.Add(treeNode);
+            }
+
+#endif
+            return bGeFunden;
+        }
+
+        private void btnSuchen_Click(object sender, EventArgs e)
+        {
+            // TODO: stack up the Alter Suche Result at specific Window or etc.
+            string strSucheText = SuchenBegriff.Text;
+
+            if(bMultiThread.Checked)    DoSearchInThread(strSucheText);
+            else                        DoSearch(strSucheText);
+
+            /*
+
+            Parallel.ForEach<ListViewItem, ListViewItem>(
+                listFiles.Items,
+                ()=> new ListViewItem() ,
+                (item, loop, final) =>
+                {
+                    string fileName = item.SubItems[0].Text;
+                    string path = item.SubItems[1].Text;
+
+                    Excel._Workbook oWB = oXL.Workbooks.Open(System.IO.Path.Combine(path, fileName), ReadOnly: true);
+                    //Excel._Workbook oWB = app.Workbooks.Open(path + "\\" + fileName);                
+                    if (null == oWB)
+                        return final;
+
+                    if (ExcelFindInWorkbook(oWB, strSucheText))
+                    {
+                        //oXL.Visible = true;
+                        //oXL.UserControl = true;
+                    }
+                    oWB.Close(SaveChanges: false);
+
+                    return final;
+                },
+                (final) => { }
+                );
+             */
+
+        }
+
+        private void DoSearch(string strSucheText)
+        {
+
+            Excel.Application oXL = new Excel.Application();
+            using (ExcelAppHolder oHolder = new ExcelAppHolder(oXL))
+                foreach (ListViewItem item in listFiles.Items)
+                {
+                    string fileName = item.SubItems[0].Text;
+                    string path = item.SubItems[1].Text;
+
+                    Excel._Workbook oWB = oXL.Workbooks.Open(System.IO.Path.Combine(path, fileName), ReadOnly: true);
+                    //Excel._Workbook oWB = app.Workbooks.Open(path + "\\" + fileName);                
+                    if (null == oWB)
+                        continue;
+
+
+                    if (ExcelFindInWorkbook(oWB, strSucheText))
+                    {
+                        //oXL.Visible = true;
+                        //oXL.UserControl = true;
+                    }
+                    oWB.Close(SaveChanges: false);
+                }
+
+        }
+
+        private void DoSearchInThread(string strSucheText)
+        {
+#if true // Multiple Search in Apolications
+            //collect the Filepaths
+            List<string> strFileList = new List<string>();
+            foreach (ListViewItem item in listFiles.Items)
+            {
+                string fileName = item.SubItems[0].Text;
+                string path = item.SubItems[1].Text;
+
+                strFileList.Add(System.IO.Path.Combine(path, fileName));
+            }
+
+            if (0 > strFileList.Count)
+                return;
+
+            TreeNode toAdd = new TreeNode("SucheAngriff:" + strSucheText);
+            //System.Threading.SpinLock locker = new System.Threading.SpinLock();
+            //bool bTake = false;
+            object sync = new object();
+
+            Parallel.ForEach<string, TreeNode>(
+                strFileList,
+                () => null,
+                (strPath, loop, childNode) =>
+                {
+                    Excel.Application oXL = new Excel.Application();
+                    using (ExcelAppHolder holderAPP = new ExcelAppHolder(oXL))
+                    {
+                        Excel._Workbook oWB = oXL.Workbooks.Open(strPath, ReadOnly: true);
+                        if (null == oWB)
+                            return null;
+                        WorkbookHolder holderWB = new WorkbookHolder(oWB);
+
+                        //DONE: Generate in Threadversion
+                        if (ExcelFindInWorkbook(ref childNode, oWB, strSucheText))
+                        {
+                            //oXL.Visible = true;
+                            //oXL.UserControl = true;
+                        }
+                        oWB.Close(SaveChanges: false);
+                    }
+
+                    return childNode;
+                },
+                (childNode) =>
+                {
+                    if (null != childNode)
+                        lock (sync)
+                        {
+                            toAdd.Nodes.Add(childNode);
+                        }
+#if false
+                //Lock here
+                try
+                {
+                    //locker.Enter(ref bTake);
+                    {
+                        //lock (sync);
+                        if (null != childNode) toAdd.Nodes.Add(childNode);
+                    };
+                }
+                finally
+                {
+                    //locker.Exit();
+                }
+
+#endif
+                }
+            );
+
+#endif
+
+#if false //Multiple search in Workbook, failure
+            List<Excel._Workbook> listWorkBook = new List<Excel._Workbook>();
+            Excel.Application oXL = new Excel.Application();
+            ExcelAppHolder oHolder = new ExcelAppHolder(oXL);
+            TreeNode toAdd = new TreeNode("SucheAngriff:" + strSucheText);
+
+            foreach (ListViewItem item in listFiles.Items)
+            {
+                string fileName = item.SubItems[0].Text;
+                string path = item.SubItems[1].Text;
+
+                Excel._Workbook oWB = oXL.Workbooks.Open(System.IO.Path.Combine(path, fileName), ReadOnly: true);
+                //Excel._Workbook oWB = app.Workbooks.Open(path + "\\" + fileName);                
+                if (null == oWB)
+                    continue;
+
+                WorkbookHolder holderWB = new WorkbookHolder(oWB);
+                listWorkBook.Add(oWB);
+            }
+            Parallel.ForEach<Excel._Workbook, TreeNode>
+                (
+                    listWorkBook,
+                    () => null,
+                    (iObjWB, loop, childNode) =>
+                    {
+                        if (ExcelFindInWorkbook(ref childNode, iObjWB, strSucheText))
+                        {
+                            //oXL.Visible = true;
+                            //oXL.UserControl = true;
+                        }
+                        iObjWB.Close(SaveChanges: false);
+                        //oXL.Quit();
+
+                        return childNode;
+                    },
+                    (childNode) =>
+                    {
+                        //Lock here
+                        try
+                        {
+                            //locker.Enter(ref bTake);
+                            {
+                                //lock (sync);
+                                if (null != childNode) toAdd.Nodes.Add(childNode);
+                            };
+                        }
+                        finally
+                        {
+                            //locker.Exit();
+                        }
+                    }
+               );
+
+#endif
+
+            treeResult.Nodes.Add(toAdd);
+
+        }
+
+        abstract class ObjHolder<T> : IDisposable
+        {
+            protected T oObj;
+
+            protected ObjHolder(T rhs)
+            {
+                oObj = rhs;
+            }
+            ~ObjHolder()
+            {
+            }
+
+            public abstract void Dispose();
+        };
+
+        class ExcelAppHolder : ObjHolder<Excel.Application>
+        {
+            public ExcelAppHolder(Excel.Application rhs) : base(rhs)
+            {
+            }
+
+            public override void Dispose()
+            {
+                oObj.Quit();
+            }
+        }
+
+        class WorkbookHolder : ObjHolder<Excel._Workbook>
+        {
+            public WorkbookHolder(Excel._Workbook rhs) : base(rhs)
+            {
+            }
+
+            public override void Dispose()
+            {
+                oObj.Close(SaveChanges: false);
+            }
+        }
+
+        private void treeResult_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            TreeNode node = treeResult.SelectedNode;
+            if (null == node.Parent) //nur Arbeit in Child Node
+                return;
+
+            if (false == (node.Parent is TreeNodeExt))
+                return;
+
+            string strRef = node.Name;
+            string strFilePath= ((TreeNodeExt)node.Parent).GetFilePath();
+
+            Excel.Application oXL = new Excel.Application();
+            Excel._Workbook oWB = oXL.Workbooks.Open(strFilePath);
+            using (ExcelAppHolder holder = new ExcelAppHolder(oXL))
+            using (WorkbookHolder wHolder = new WorkbookHolder(oWB))
+                do
+                {
+                    if (null == oWB)
+                        break;
+
+                    /*
+                    Excel._Worksheet oSheet = oWB.Sheets[];
+                    Excel.Range oRG = oSheet.get_Range();
+                    */
+                 }while(false);
+
+        }
+    }
+}
